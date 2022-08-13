f1d902c83c059606c43bc8870b3c22d5d0df6a75
David Augusto <58609328+david-augusto@users.noreply.github.com>
2022-08-13 15:09:32 -0300

    Add files via upload

---
+++ 01-LOG-PRO-ORI-OBJ/04-PRO-ORI-OBJ/09-PAC/br/com/empresa/segundo_setor/Veiculo.java
@@ -0,0 +1,14 @@
+package br.com.empresa.segundo_setor;
+
+public class Veiculo {
+
+	public Veiculo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 01-LOG-PRO-ORI-OBJ/04-PRO-ORI-OBJ/09-PAC/br/com/empresa/setor/Pessoa.java
@@ -0,0 +1,14 @@
+package br.com.empresa.setor;
+
+public class Pessoa {
+
+	public Pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
