863a11f7d5cd2c4063eb97c49ec99332a1240df4
David Augusto <58609328+david-augusto@users.noreply.github.com>
2022-08-17 11:58:40 -0300

    Update LerTabelas.py

--- 01-LOG-PRO-ORI-OBJ/02-INT-PRO/13-ACE-TAB-LIS/LerTabelas.py
+++ 01-LOG-PRO-ORI-OBJ/02-INT-PRO/13-ACE-TAB-LIS/LerTabelas.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("G:\\Outros computadores\\Meu modelo Laptop\\Backup Notebook\\13 - Softex\\01 - LOGICA DE PROGRAMACAO ORIENTADA A OBJETOS\\MODULO 02 - INTRODUCAO A PROGRAMACAO\\13 - ACESSAR TABELAS E LISTAS\\notas_alunos.csv", "r") as arquivo:
+with open("notas_alunos.csv", "r") as arquivo:
 	arquivo_csv = csv.reader(arquivo, delimiter=",")
 	for linha in arquivo_csv:
 		print(linha)
